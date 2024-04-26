7e5393ea63121c6b508ff8c7991286f9919ec027
mrxdoffical <berogergesg@gmail.com>
2024-04-26 20:14:43 +0300

    fix js

--- logics/main.js
+++ logics/main.js
@@ -1,18 +1,14 @@
 const txt1 = document.getElementById('userInput');
 const btn1 = document.getElementById('button1');
 const txt2 = document.getElementById('output');
-function fun1()
-{
-	txt1.value;
-	if (txt1.value == "yes")
-	{
-		txt2.innerHTML = "congrats you are mine now"
-	}
-	else
-	{
+function fun1() {
+    if (txt1.value.toLowerCase() === "yes") {
+        txt2.innerHTML = "congrats you are mine now";
+    } else {
         txt2.innerHTML = "مش بمزاجك يبقرتي";
     }
 }
+
 
 
 btn1.addEventListener('click',fun1);
