fe0e1e276759e9771e9ce91739a807b0a0af415c
mrxdoffical <berogergesg@gmail.com>
2024-04-26 19:31:48 +0300

    added tasks

--- logics/main.js
+++ logics/main.js
@@ -6,10 +6,11 @@ function fun1()
 	txt1.value;
 	if (txt1.value == "yes")
 	{
-		txt2.innerHTML = "congrats you are mine now";
-	}else
+		txt2.innerHTML = "congrats you are mine now"
+	}
+	else
 	{
-        txt2.innerHTML = "u don't have another option \nhint \"yes\"";
+        txt2.innerHTML = "مش بمزاجك يبقرتي";
     }
 }
 
